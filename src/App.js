2c1ecb839b06e150741e79f84da62b5dce4bc0e1
norielcabacungan <norielcabacungan@gmail.com>
2022-04-06 13:25:37 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import 'assets/css/vendor.bundle.css?ver=1930';
-import 'assets/css/style-lobelia.css?ver=1930';
-import 'ssets/css/theme.css?ver=1930';
+
 
 function App() {
   return (
